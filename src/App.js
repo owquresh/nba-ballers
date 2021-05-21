b343838cf838b9b3255490ebfb2bc5a9ed47bbd2
Omar W. Qureshi <owquresh@usc.edu>
2021-05-20 19:06:24 -0700

    Basic boilerplate added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar.js'
+import {ListGroup} from 'react-bootstrap';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+  		players: [],
+  		pageCount: 0,
+  		pageCurrentNumber: 1,
+  		per_page: 25,
+  		isLoading: true,
+  		};
+  	
+  		this.getData = this.getData.bind(this);
+  	}
+
+	
+
+  	
+
+  
+
+
+   displayPlayers = () => {
+   		const players = this.state.players;
+   		console.log(players)
+   		console.log("test")
+   	}
+
+	//Grab data from NBA ball dont lie API
+	getData(){
+		axios.get('https://www.balldontlie.io/api/v1/players')
+			.then(res => {
+				const players= res.data;
+				this.setState({players, isLoading: false}, ()=>{
+					console.log(this.state.players.data)
+				});
+				const pageCount = res.data.meta.total_pages;
+				this.setState({pageCount})
+				
+			})
+	}
+
+    componentDidUpdate(){
+
+	}
+
+	componentDidMount(_){
+	  	this.getData();
+	  }
+
+    render(){
+    	const{isLoading} = this.state
+    	if(isLoading){
+    		return null;
+    	}
+
+	  return (
+	    <div className="App">
+
+	     <SearchBar />
+	     <ListGroup horizontal>
+	     	{this.state.players.data.map((player)=>(
+				<Player  
+					firstName={player.first_name}
+					lastName={player.last_name}
+					height={player.height_feet}
+					position={player.position}
+					team={player.team}
+				/>
+			))}
+	     </ListGroup>
+	    </div>
+
+	 
+	  )
+	}
 }
+
+const Player = ({firstName, lastName, height, position, team}) => (
+		<ListGroup.item>
+			<p>Name:{firstName} {lastName}</p>
+			<p>{height}</p>
+			<p>{position}</p>
+			<p>{team}</p>
+		</ListGroup.item>
+);
+
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+
+class SearchBar extends React.Component {
+	render() {
+		return(
+			<Container>
+				<Row>
+					<Col>Search Bar</Col>
+					<Col>Tab Viewer</Col>
+				</Row>
+			</Container>
+			
+		);	
+	}
+
+}
+export default SearchBar;
