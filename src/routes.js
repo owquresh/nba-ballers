09a7a78acc728d8b105f75a8f53211e7a5d19daa
Omar W. Qureshi <owquresh@usc.edu>
2021-05-24 11:02:34 -0700

    Add routes and teampage

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+
+import App from 'App.js';
+import TeamPage from './components/MainPage';
+
+export default (
+	<Route path=></Route>
+
+
+)
